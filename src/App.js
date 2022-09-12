6854a1872823bddce0e66be074b626d2ca90b8a4
Simeon Georgiev <sgeorgiev783@gmail.com>
2022-09-12 15:37:24 +0300

    added basic routing and few components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import { Box } from '@material-ui/core'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
 
 function App() {
+  console.log(Box)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Box width='400px'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/exercise/:id' element={<ExerciseDetail />} />
+        </Routes>
+        <Footer />
+      </Box>
+    </>
   );
 }
 
